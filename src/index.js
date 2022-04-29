55bcaa8602fc284abf7ed7d1852f10c252bbb5cd
tomo <61913268+tomo1833@users.noreply.github.com>
2022-04-29 21:22:36 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div>
+  <span class="ball">
+    <span class="small-ball-eye-left"></span>
+    <span class="small-ball-eye-left-center"></span>
+    <span class="small-ball-eye-right"></span>
+    <span class="small-ball-eye-right-center"></span>
+  </span>
+  <span class="shadow"></span>
+</div>
+`;
